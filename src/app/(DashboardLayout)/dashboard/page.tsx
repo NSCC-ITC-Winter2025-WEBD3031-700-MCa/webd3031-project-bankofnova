64f9db455dbedd76795f1f21c2d9ca69be10b070
Max Peach <maxpeach742@gmail.com>
2025-04-02 13:45:20 -0300

    feat: improvements

--- src/app/(DashboardLayout)/dashboard/page.tsx
+++ src/app/(DashboardLayout)/dashboard/page.tsx
@@ -8,7 +8,7 @@ import ProductPerformance from '../components/dashboard/ProductPerformance';
 import Blog from '../components/dashboard/Blog';
 import MonthlyEarnings from '../components/dashboard/MonthlyEarnings';
 import AllUsers from '../components/dashboard/AllUsers';
-//
+// ttt
 const Dashboard = () => {
   return (
     <PageContainer title="Dashboard" description="this is Dashboard">
